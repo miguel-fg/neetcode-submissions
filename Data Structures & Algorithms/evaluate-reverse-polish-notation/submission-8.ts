611b31d57322a4b702a45f5b4b143b6c8e0200a8
Miguel Fierro <72409412+miguel-fg@users.noreply.github.com>
2026-08-19 13:08:21 -0700

    Add: evaluate-reverse-polish-notation - submission-8

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-8.ts
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens: string[]): number {
+        const ops: Set<string> = new Set(["+", "-", "*", "/"]) 
+        const nums: number[] = []
+
+        for(const t of tokens){
+            if (ops.has(t)){
+                const n2 = nums.pop()
+                const n1 = nums.pop()
+                const res = this.eval(n1, n2, t)
+                nums.push(res)
+            } else {
+                nums.push(parseInt(t))
+            }
+        }
+
+        return nums.pop()
+    }
+
+    eval(n1: number, n2: number, t: string): number {
+        switch(t){
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "*":
+                return n1 * n2
+            default:
+                return Math.trunc(n1 / n2)
+        }
+    }
+}
